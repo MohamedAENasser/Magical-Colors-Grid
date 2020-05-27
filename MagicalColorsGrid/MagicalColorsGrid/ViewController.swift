7ac5b918bcd1cdc2da57b2aa5aeeb6506e49ef73
Mohamed Salah ElDien <mohamed.salaheldienhamed@vodafone.com>
2020-05-27 18:47:41 +0200

    Initial commit

---
+++ MagicalColorsGrid/MagicalColorsGrid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicalColorsGrid
+//
+//  Created by Mohamed Abd ElNasser on 5/27/20.
+//  Copyright © 2020 MohamedAENasser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
